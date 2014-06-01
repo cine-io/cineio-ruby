71c26ddbd27e4c908d9e2e3f1df207ebbb1093d9
Thomas Shafer <thomasjshafer@gmail.com>
2014-06-01 16:04:51 -0700

    added streams/index call

--- lib/cine_io/streams_handler.rb
+++ lib/cine_io/streams_handler.rb
@@ -1,5 +1,9 @@
 class CineIo::StreamsHandler < CineIo::ResourceHandler
 
+  def index
+     get_resource("/streams").map(&CineIo::Stream.method(:new))
+  end
+
   def get(stream_id)
     CineIo::Stream.new get_resource("/stream", id: stream_id)
   end
--- spec/cine_io/streams_handler_spec.rb
+++ spec/cine_io/streams_handler_spec.rb
@@ -25,6 +25,27 @@ describe CineIo::StreamsHandler do
     end
   end
 
+  describe '#streams' do
+    it "returns the streams" do
+      VCR.use_cassette('get_streams') do
+        streams = subject.index
+        expect(streams).to have(1).item
+        stream = streams.first
+        expect(stream).to be_a(CineIo::Stream)
+        expect(stream.id).to eq("53718cef450ff80200f81856")
+        expect(stream.password).to eq("THEPASSWORD")
+        expect(stream.play.keys.sort).to eq(['hls', 'rtmp'])
+        expect(stream.publish.keys.sort).to eq(['stream', 'url'])
+      end
+    end
+
+    it 'can throw an error on api exception' do
+      VCR.use_cassette('get_streams_error', record: :once) do
+        expect {subject.index}.to raise_error(CineIo::ApiError, "An unknown error has occured.")
+      end
+    end
+  end
+
   describe '#create' do
     it "returns a stream" do
       VCR.use_cassette('create_stream') do
