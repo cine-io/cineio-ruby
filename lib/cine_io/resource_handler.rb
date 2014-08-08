f1649ef12f13eefdd17198943a200f9f8978b49f
Thomas Shafer <thomasjshafer@gmail.com>
2014-08-08 10:40:28 -0700

    added cineio-ruby useragent to api requests

--- lib/cine_io/resource_handler.rb
+++ lib/cine_io/resource_handler.rb
@@ -2,6 +2,8 @@ class CineIo::ResourceHandler
 
   protected
 
+  USER_AGENT = "cineio-ruby version-#{CineIo::VERSION}"
+
   def initialize(client)
     @client = client
   end
@@ -10,8 +12,9 @@ class CineIo::ResourceHandler
     uri = URI.parse("#{CineIo::BASE_URL}#{path}")
     uri.query = URI.encode_www_form({:secretKey => @client.config.fetch(:secretKey)}.merge(params))
     http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Get.new(uri.request_uri)
     http.use_ssl = (uri.scheme == "https")
+
+    request = Net::HTTP::Get.new(uri.request_uri, {"User-Agent" => USER_AGENT})
     response = http.request(request)
 
     raise CineIo::ApiError.new(response.body) unless response.is_a?(Net::HTTPSuccess)
@@ -20,10 +23,11 @@ class CineIo::ResourceHandler
 
   def post_resource(path, params={})
     uri = URI.parse("#{CineIo::BASE_URL}#{path}")
-    request = Net::HTTP::Post.new(uri.request_uri)
-    request.set_form_data({:secretKey => @client.config.fetch(:secretKey)}.merge(params))
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = (uri.scheme == "https")
+
+    request = Net::HTTP::Post.new(uri.request_uri, {"User-Agent" => USER_AGENT})
+    request.set_form_data({:secretKey => @client.config.fetch(:secretKey)}.merge(params))
     response = http.request(request)
 
     raise CineIo::ApiError.new(response.body) unless response.is_a?(Net::HTTPSuccess)
@@ -32,10 +36,11 @@ class CineIo::ResourceHandler
 
   def update_resource(path, params={})
     uri = URI.parse("#{CineIo::BASE_URL}#{path}")
-    request = Net::HTTP::Put.new(uri.request_uri)
-    request.set_form_data({:secretKey => @client.config.fetch(:secretKey)}.merge(params))
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = (uri.scheme == "https")
+
+    request = Net::HTTP::Put.new(uri.request_uri, {"User-Agent" => USER_AGENT})
+    request.set_form_data({:secretKey => @client.config.fetch(:secretKey)}.merge(params))
     response = http.request(request)
 
     raise CineIo::ApiError.new(response.body) unless response.is_a?(Net::HTTPSuccess)
@@ -44,10 +49,11 @@ class CineIo::ResourceHandler
 
   def delete_resource(path, params={})
     uri = URI.parse("#{CineIo::BASE_URL}#{path}")
-    request = Net::HTTP::Delete.new(uri.request_uri)
-    request.set_form_data({:secretKey => @client.config.fetch(:secretKey)}.merge(params))
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = (uri.scheme == "https")
+
+    request = Net::HTTP::Delete.new(uri.request_uri, {"User-Agent" => USER_AGENT})
+    request.set_form_data({:secretKey => @client.config.fetch(:secretKey)}.merge(params))
     response = http.request(request)
 
     raise CineIo::ApiError.new(response.body) unless response.is_a?(Net::HTTPSuccess)
